9a57693dbecd7aaa077322a302a192d475500a94
Andrey Koval <akoval29@gmail.com>
2024-09-02 23:47:41 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-"use strict";
-
-// цей скріпт продивляється весь код на предмет div, в яких є id
-// (впринципі існує id). Потім він генерує елементи з тегом <a>, 
-// яким присвоює href із value (getAttribute) тих id, які він знайшов, 
-// це для прокрутки до самих елементів. Ну і звук кліка на <a>
-// enjoy !!!!!!
-
-const clickSound = new Audio('src/bup.ogg'); // muz
-const divs = document.querySelectorAll('[id]'); // знаходимо ВСІ div, які мають id
-const navEl = document.querySelector('.page__1'); // знаходимо секцію куди будем генерувати <a>
+const clickSound = new Audio("src/bup.ogg"); // muz
+const divs = document.querySelectorAll("[id]"); // знаходимо ВСІ div, які мають id
+const navEl = document.querySelector(".page__1"); // знаходимо секцію куди будем генерувати <a>
 for (let i = 0; i < divs.length; i++) {
-  const id = divs[i].getAttribute('id'); // забираєм attribute
+  const id = divs[i].getAttribute("id"); // забираєм attribute
 
-  const a = document.createElement('a');
-  a.setAttribute('href', `#${id}`); // вставляєм attribute в href
+  const a = document.createElement("a");
+  a.setAttribute("href", `#${id}`); // вставляєм attribute в href
   a.textContent = `Display: ${id}`;
 
-  a.addEventListener("click", () => { // накидуєм звук кліка
+  a.addEventListener("click", (event) => {
+    // накидуєм звук кліка
+    event.preventDefault(); // відміняємо стандартну дію
+
+    const targetElement = document.querySelector(`#${id}`);
+    const offset = 90; // Зсув на 60 пікселів
+    const elementPosition =
+      targetElement.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+
     clickSound.currentTime = 0;
     clickSound.play();
   });
